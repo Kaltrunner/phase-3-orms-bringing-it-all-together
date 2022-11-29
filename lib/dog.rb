228cb4d03534d51469ff6d719840dda21d3f4e11
Erik Russell <erikrussells@gmail.com>
2022-11-29 10:22:53 -0500

    commit tree d6c8a8281e33444d27287f6daecf105cf0da4815

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
 
+    describe ".create_table" do
+        it 'creates the dogs table in the database' do
+          DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+          Dog.create_table
+          table_check_sql = "SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='dogs';"
+          expect(DB[:conn].execute(table_check_sql)[0]).to eq(['dogs'])
+        end
+      end
+
 end
